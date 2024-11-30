3cc65656faac85cb8300519662cb9aaa9fc86790
박민규 <gksktl111@naver.com>
2024-11-30 18:02:34 +0900

    first commit

---
+++ section2/src/chapter2.ts
@@ -0,0 +1,17 @@
+// ** 배열
+let arr: number[] = [1, 2, 3];
+
+let boolArr: Array<boolean> = [true, false, true];
+
+// ** 유니온 타입 (number | string)
+let tuple: (number | string)[] = [1, "2"];
+
+
+// ** 다차원 배열을 정의하는 방법
+let multiArr: number[][] = [[1, 2], [3, 4], [5, 6]];
+
+
+// ** 튜플
+// ** 길이와 타입이 고정된 배열
+
+let tup1: [number, string] = [1, "2"];
---
+++ section2/src/chapter3.ts
@@ -0,0 +1,17 @@
+// ** 객체 리터럴 타입
+
+let user : {
+    id : number,
+    name : string
+} = {
+    id : 1,
+    name : "박민규"
+}
+
+let dog : {
+    name : string,
+    color : string
+} = {
+    name : "멍멍이",
+    color : "하얀색"
+}
