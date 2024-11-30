c06eb0bdee07db8e49f27c456e802fe47f4b01ba
gksktl111 <gksktl111@naver.com>
2024-11-30 18:10:01 +0900

    커밋 테스트

--- section2/src/chapter3.ts
+++ section2/src/chapter3.ts
@@ -1,17 +1,17 @@
 // ** 객체 리터럴 타입
 
-let user : {
-    id : number,
-    name : string
+let user: {
+  id: number;
+  name: string;
 } = {
-    id : 1,
-    name : "박민규"
-}
+  id: 1,
+  name: '박민규',
+};
 
-let dog : {
-    name : string,
-    color : string
+let dog: {
+  name: string;
+  color: string;
 } = {
-    name : "멍멍이",
-    color : "하얀색"
-}
+  name: '멍멍이',
+  color: '하얀색',
+};
